c830887e62681a41e570a0cd01e3fd426727dec5
Jinwook Park <jinwookpark2296@gmail.com>
2024-11-12 13:17:11 +0900

    feat: Add reading module for POS tag in LPF
    * Labeled Parse-Focusing support POS tag reading in parse trees now.

--- parser/model/LPFTNPCFG.py
+++ parser/model/LPFTNPCFG.py
@@ -2,7 +2,6 @@ from collections import defaultdict, Counter
 
 import torch
 
-from parser.model.FTNPCFG import FTNPCFG
 from parser.model.TNPCFG import TNPCFG
 from parser.model.PFTNPCFG import Parse_Focusing
 
@@ -11,7 +10,8 @@ class Labeled_Parse_Focusing(Parse_Focusing):
     def prepare_trees(self, model_paths):
         self.parse_trees = []
         # self.parse_labels = []
-        self.labels = Counter()
+        nts = Counter()
+        ts = Counter()
         for model in model_paths:
             parses = defaultdict(list)
             with open(model, "rb") as f:
@@ -19,26 +19,40 @@ class Labeled_Parse_Focusing(Parse_Focusing):
             for t in pts:
                 n_word = t["word"]
                 label = t["label"]
+                # Get trees
                 if "pred_tree" in t.keys():
                     n_tree = [s for s in t["pred_tree"] if s[1] - s[0] > 1]
                 elif "tree" in t.keys():
                     n_tree = [s[:2] for s in t["tree"] if s[1] - s[0] > 1]
                 n_tree = [(t[0], t[1], l) for t, l in zip(n_tree, label)]
-
                 parses[str(n_word)] = n_tree
                 # self.label_set.update(label)
                 for l in label:
-                    self.labels[l] += 1
+                    nts[l] += 1
+                # Add pos spans
+                if "pos" in t.keys():
+                    pos = t["pos"]
+                    n_tree += [(i, i + 1, p) for i, p in enumerate(pos)]
+                    for p in pos:
+                        ts[p] += 1
+
             self.parse_trees.append(parses)
 
         # Indexing labels
-        self.label2idx = {
-            l[0]: i for i, l in enumerate(self.labels.most_common())
-        }
+        self.idx2nt = [l for l, c in nts.most_common()]
+        self.nt2idx = {l[0]: i for i, l in enumerate(nts.most_common())}
+        self.idx2t = [p for p, c in ts.most_common()]
+        self.t2idx = {p[0]: i for i, p in enumerate(ts.most_common())}
 
         for pts in self.parse_trees:
             for k, t in pts.items():
-                pts[k] = [(s[0], s[1], self.label2idx[s[2]]) for s in t]
+                n_tree = []
+                for s in t:
+                    if s[1] - s[0] > 1:
+                        n_tree += [(s[0], s[1], self.nt2idx[s[2]])]
+                    else:
+                        n_tree += [(s[0], s[1], len(nts) + self.t2idx[s[2]])]
+                pts[k] = n_tree
 
     def get_pretrained_tree_mask(self, words):
         b, seq_len = words.shape[:2]
